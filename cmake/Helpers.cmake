dc072188cf1143bc9de4b403c5670eb7f9395ef1
Jakub Dobrowolski <jd.kuba96@gmail.com>
2025-08-06 17:13:21 +0200

    [0.0.1] Feature/basic setup (#1)
    * Adding library separation
    Adding basic test for checking pybinds
    adding Placeholder README.md
    Adding docker files for development docker images
    testing cicd with cuda from Jimver
    
    * Updating check cuda step
    
    * Checking windows cuda instalation
    
    * Changing cuda toolkit cicd version to v0.2.24
    
    * Adjusting cuda version to cuda toolkit 0.2.24 version
    
    * Leaving only linux workflow for now. Windows one is buggy and not working with newest version of cuda toolkit https://github.com/Jimver/cuda-toolkit/issues/395
    
    * Trying to unlock concurrent jobs execution in cicd for linux and cuda artifacts error
    
    * Trying to make windows cicd to work
    
    * Updating cuda toolkit on windows to fix path validation error
    
    * Trying to fix no cuda toolset found on windows cicd
    
    * checking python libs
    
    * Trying to give python3 root dir path to cmake configure in windows cicd
    
    * Trying to add Python3 Library to cmake and veryfying it by printing message
    
    * Removing double backslash to test
    
    * Removing windows cicd for now. Technical debt i guess

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -1,34 +1,84 @@
+function(add_app APP_NAME)
+    cmake_parse_arguments(ARGS "" "" "SOURCES;DEPENDS" ${ARGN})
+    add_executable(${APP_NAME} ${ARGS_SOURCES})
+    get_property(PROJECT_LIBS GLOBAL PROPERTY PROJECT_LIBS_PROPERTY)
+    target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include)
+    find_and_link_libs(${APP_NAME} DEPENDS ${ARGS_DEPENDS})
+endfunction()
+
 function(package_add_test TESTNAME)
     cmake_parse_arguments(ARGS "${options}" "" "SOURCES;DEPENDS" ${ARGN})
-    project_options(
-            PREFIX "cpptemplatetest"
-            ENABLE_CACHE
-            ${ENABLE_CPPCHECK}
-            ${ENABLE_CLANG_TIDY}
-            WARNINGS_AS_ERRORS
-    )
     add_executable(${TESTNAME} ${ARGS_SOURCES})
-    target_link_libraries(${TESTNAME}
-            PRIVATE cpptemplatetest_project_options cpptemplatetest_project_warnings
-    )
     target_include_directories(${TESTNAME} PUBLIC ${CMAKE_SOURCE_DIR}/include)
-    find_and_link_libs(${TESTNAME})
+    find_and_link_test_libs(${TESTNAME})
     gtest_discover_tests(${TESTNAME}
             WORKING_DIRECTORY ${PROJECT_DIR}
             PROPERTIES VS_DEBUGGER_WORKING_DIRECTORY "${PROJECT_DIR}"
     )
 endfunction()
 
+function(add_library_and_link)
+    set(prefix ARG)
+    set(options SKIP_CXX_FLAGS)
+    set(singleValues NAME)
+    set(multiValues SOURCES DEPENDS)
+    cmake_parse_arguments(${prefix} "${options}" "${singleValues}" "${multiValues}" ${ARGN})
+    if (ARG_SKIP_CXX_FLAGS)
+        set(SKIP_CXX_FLAGS "SKIP_CXX_FLAGS")
+    endif ()
+
+    set(LIB_NAME ${${prefix}_NAME})
+    set(LIB_SOURCES ${${prefix}_SOURCES})
+    add_library(${LIB_NAME} STATIC ${LIB_SOURCES})
+
+    find_and_link_libs(${LIB_NAME} DEPENDS ${${prefix}_DEPENDS})
+endfunction()
+
 function(find_and_link_libs TARGET_NAME)
+    cmake_parse_arguments(ARGS "" "" "DEPENDS" ${ARGN})
+    target_include_directories(${TARGET_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/include)
     target_find_dependencies(${TARGET_NAME}
             PRIVATE_CONFIG
             fmt
+            spdlog
+            pybind11
+    )
+
+    target_link_system_libraries(${TARGET_NAME}
+            PRIVATE
+            fmt::fmt
+            spdlog::spdlog
+
+    )
+
+    target_link_libraries(${TARGET_NAME}
+            PRIVATE knet_project_options knet_project_warnings
+            PUBLIC
+            pybind11::headers
+            ${ARGS_DEPENDS}
+    )
+endfunction()
+
+function(find_and_link_test_libs TARGET_NAME)
+    target_find_dependencies(${TARGET_NAME}
+            PRIVATE_CONFIG
+            fmt
+            spdlog
+            pybind11
             GTest
     )
 
     target_link_system_libraries(${TARGET_NAME}
             PRIVATE
             fmt::fmt
+            spdlog::spdlog
             gtest::gtest
     )
+    target_link_libraries(${TARGET_NAME}
+            PRIVATE knet_project_options knet_project_warnings
+            PUBLIC
+            pybind11::headers
+            ${ARGS_DEPENDS}
+    )
+
 endfunction()
--- docker/Base.Dockerfile
+++ docker/Base.Dockerfile
@@ -1,18 +1,14 @@
-FROM ubuntu:22.04
+FROM nvcr.io/nvidia/cuda:12.9.1-cudnn-devel-ubuntu24.04
 
-RUN apt-get update && apt-get -y install sudo build-essential software-properties-common ninja-build wget libssl-dev libmpfr-dev libgmp3-dev libmpc-dev
+RUN gcc --version
 
-RUN wget http://ftp.gnu.org/gnu/gcc/gcc-15.1.0/gcc-15.1.0.tar.gz && tar -xf gcc-15.1.0.tar.gz && cd gcc-15.1.0 && ./configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=/usr/local/gcc-15.1.0 --enable-checking=release --enable-languages=c,c++ --disable-multilib --program-suffix=-15.1.0 && make && sudo make install
+RUN apt-get update && apt-get -y install sudo software-properties-common ninja-build wget libssl-dev
 
 RUN cd /tmp &&  \
-    wget https://github.com/Kitware/CMake/archive/refs/tags/v3.30.2.tar.gz &&  \
-    tar -zxvf v3.30.2.tar.gz
+    wget https://github.com/Kitware/CMake/archive/refs/tags/v4.0.3.tar.gz &&  \
+    tar -zxvf v4.0.3.tar.gz
 
-RUN cd /tmp/CMake-3.30.2 &&  \
+RUN cd /tmp/CMake-4.0.3 &&  \
     ./bootstrap &&  \
     sudo gmake &&  \
     sudo gmake install
-
-RUN update-alternatives --install /usr/bin/g++ g++ /usr/local/gcc-14.2.0/bin/g++-15.1.0 15
-RUN update-alternatives --install /usr/bin/gcc gcc /usr/local/gcc-14.2.0/bin/gcc-15.1.0 15
-RUN gcc --version
